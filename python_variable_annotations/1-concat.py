229d37952f97848e56aead2ce82516a4695ffaa2
salman_ghannam1 <sloome222@gmail.com>
2026-04-15 15:10:16 +0300

    f

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module for string concatenation function."""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Returns the concatenation of two strings."""
+    return str1 + str2
