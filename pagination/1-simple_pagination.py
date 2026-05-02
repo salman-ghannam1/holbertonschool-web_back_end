b82ffaab82ffd4ada5f60729eee78b63507ab05d
salman_ghannam1 <sloome222@gmail.com>
2026-05-02 03:48:03 +0300

    f

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -43,8 +43,7 @@ class Server:
 
         return self.__dataset
 
-    def get_page(self, page: int = 1,
-                 page_size: int = 10) -> List[List]:
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """
         Return the requested page of the dataset.
         """
