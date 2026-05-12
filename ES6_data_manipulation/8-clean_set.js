65c63aadda9defbbe62223b3f0032672de358528
salman_ghannam1 <sloome222@gmail.com>
2026-05-12 12:50:39 +0300

    f

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,3 +1,10 @@
-export default function hasValuesFromArray(set, array) {
-  return array.every((value) => set.has(value));
+export default function cleanSet(set, startString) {
+  if (typeof startString !== 'string' || startString.length === 0) {
+    return '';
+  }
+
+  return [...set]
+    .filter((value) => value.startsWith(startString))
+    .map((value) => value.slice(startString.length))
+    .join('-');
 }
