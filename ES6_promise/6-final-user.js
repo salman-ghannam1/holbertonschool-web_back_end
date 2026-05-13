07c1e847f995e627ae4ee56303f48802ab299370
salman_ghannam1 <sloome222@gmail.com>
2026-05-13 12:31:32 +0300

    f

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,13 +1,18 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(
-  firstName,
-  lastName,
-  fileName
-) {
+export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ]);
+  ]).then((results) => results.map((result) => {
+    if (result.status === 'rejected') {
+      return {
+        status: result.status,
+        value: result.reason.toString(),
+      };
+    }
+
+    return result;
+  }));
 }
