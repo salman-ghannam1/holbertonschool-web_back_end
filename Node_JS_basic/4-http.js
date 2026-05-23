f0ee8d2aea326255927e98621cab389f9b3f90f6
salman_ghannam1 <sloome222@gmail.com>
2026-05-23 16:23:18 +0300

    f

---
+++ Node_JS_basic/4-http.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const app = http.createServer((request, response) => {
+  response.statusCode = 200;
+  response.setHeader('Content-Type', 'text/plain');
+  response.end('Hello Holberton School!');
+});
+
+app.listen(1245);
+
+module.exports = app;
